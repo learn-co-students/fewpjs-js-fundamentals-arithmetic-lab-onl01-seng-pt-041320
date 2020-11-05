ea83aa55aa5b8a2560636a42c11e898948efc6d6
Andrew Alteri <andrewsalteri@gmail.com>
2020-11-05 15:02:55 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random() *10) + 1;
+
+let mod = 19 % 5;
+
+let max = Math.max(5, 10, 15, 20);
