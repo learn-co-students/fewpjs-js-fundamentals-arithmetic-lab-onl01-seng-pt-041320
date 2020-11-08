cfa061e2e9f873211bcb11340237c8f65f8708bb
angieappiah <alegnalawn2@yahoo.com>
2020-11-07 16:47:03 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = (5,10,15,20);
