9e5b3dca741fad6bf98ca00f84ac1872a07a7c54
christinezosche <christine.zosche@gmail.com>
2020-11-17 10:49:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let number = Math.floor(Math.random() * 10) + 1;
+const random = number*number
+
+const num1 = 6.2
+const num2 = 10
+const multiply = num1*num2
+
+const mod = 25%7
+
+const max = Math.max(1,3,20)
